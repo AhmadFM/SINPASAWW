b13d5ec9d638443ffd1d974a2511b3348cdc5e2d
resti-3 <restifujianti01@gmail.com>
2026-06-08 11:11:52 +0700

    debuging

--- app/Http/Controllers/Tenant/PengaturanController.php
+++ app/Http/Controllers/Tenant/PengaturanController.php
@@ -59,13 +59,21 @@ class PengaturanController extends Controller
         ];
 
         /* ── Handle foto upload ── */
+        // if ($request->hasFile('foto')) {
+        //     // Hapus foto lama
+        //     if ($tenant->foto && Storage::disk('public')->exists($tenant->foto)) {
+        //         Storage::disk('public')->delete($tenant->foto);
+        //     }
+        //     $path = $request->file('foto')->store('tenant-photos', 'public');
+        //     $tenantData['foto'] = 'storage/' . $path;
+        // }
+
+        /* ── Handle foto upload ── */
         if ($request->hasFile('foto')) {
-            // Hapus foto lama
-            if ($tenant->foto && Storage::disk('public')->exists($tenant->foto)) {
-                Storage::disk('public')->delete($tenant->foto);
-            }
-            $path = $request->file('foto')->store('tenant-photos', 'public');
-            $tenantData['foto'] = 'storage/' . $path;
+            $path = cloudinary()->upload($request->file('foto')->getRealPath(), [
+                'folder' => 'tenant-photos'
+            ])->getSecurePath();
+            $tenantData['foto'] = $path;
         }
 
         $tenant->update($tenantData);
